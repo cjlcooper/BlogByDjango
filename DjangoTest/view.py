9e52ba422019c68430ab8f25fcb5336510e3d4de
MAC <mac@192.168.2.102>
2017-06-24 22:44:16 +0800

    添加app模块

--- DjangoTest/view.py
+++ DjangoTest/view.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
 
-def hello(request):
+def index(request):
 	context = {}
 	context['hello'] = 'Hello Django'
 	return render(request, 'index.html', context)
+
+	
